f51e83093c9c021d9b4bfbf34067ed905fc8653d
sujitojha1 <payalchakraborty@Payals-MacBook-Air.local>
2020-03-10 21:30:33 +0530

    train and test training modules updates

--- Modules/test_model.py
+++ Modules/test_model.py
@@ -8,32 +8,36 @@ Reference:
 import torch
 from tqdm import tqdm
 
-def test(net, device, testloader, optimizer, criterion):
+class test:
 
-    global test_losses
-    global test_acc
+    def __init__(self):
 
-    net.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(testloader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
+        self.test_losses = []
+        self.test_acc    = []
 
-            test_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
+    def execute(self, net, device, testloader, optimizer, criterion):
 
-    test_loss /= len(testloader.dataset)
-    test_losses.append(test_loss)
+        net.eval()
+        test_loss = 0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for batch_idx, (inputs, targets) in enumerate(testloader):
+                inputs, targets = inputs.to(device), targets.to(device)
+                outputs = net(inputs)
+                loss = criterion(outputs, targets)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+                test_loss += loss.item()
+                _, predicted = outputs.max(1)
+                total += targets.size(0)
+                correct += predicted.eq(targets).sum().item()
 
-    # Save.
-    test_acc.append(100. * correct / len(test_loader.dataset))
+        test_loss /= len(testloader.dataset)
+        self.test_losses.append(test_loss)
+
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+            test_loss, correct, len(testloader.dataset),
+            100. * correct / len(testloader.dataset)))
+
+        # Save.
+        self.test_acc.append(100. * correct / len(testloader.dataset))
--- Modules/train_model.py
+++ Modules/train_model.py
@@ -7,46 +7,50 @@ Reference:
 '''
 from tqdm import tqdm
 
+class train:
 
-# Training
-def train(net, device, trainloader, optimizer, criterion, epoch):
-    global train_losses
-    global train_acc
-    
-    print('\nEpoch: %d' % epoch)
-    net.train()
-    train_loss = 0
-    correct = 0
-    #total = 0
-    processed = 0
-    pbar = tqdm(trainloader)
+    def __init__(self):
 
-    for batch_idx, (inputs, targets) in enumerate(pbar):
-        # get samples
-        inputs, targets = inputs.to(device), targets.to(device)
+        self.train_losses = []
+        self.train_acc    = []
 
-        # Init
-        optimizer.zero_grad()
+    # Training
+    def execute(self,net, device, trainloader, optimizer, criterion,epoch):
 
-        # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes. 
-        # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
+        print('\nEpoch: %d' % epoch)
+        net.train()
+        train_loss = 0
+        correct = 0
+        #total = 0
+        processed = 0
+        pbar = tqdm(trainloader)
 
-        # Predict
-        outputs = net(inputs)
+        for batch_idx, (inputs, targets) in enumerate(pbar):
+            # get samples
+            inputs, targets = inputs.to(device), targets.to(device)
 
-        # Calculate loss
-        loss = criterion(outputs, targets)
-        train_losses.append(loss)
+            # Init
+            optimizer.zero_grad()
 
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
+            # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes. 
+            # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
 
-        train_loss += loss.item()
-        
-        _, predicted = outputs.max(1)
-        processed += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
+            # Predict
+            outputs = net(inputs)
 
-        pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
-        train_acc.append(100*correct/processed)
+            # Calculate loss
+            loss = criterion(outputs, targets)
+            self.train_losses.append(loss)
+
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+            
+            _, predicted = outputs.max(1)
+            processed += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+            self.train_acc.append(100*correct/processed)
