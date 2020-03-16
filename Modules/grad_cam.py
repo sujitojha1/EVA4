6070578607ef720afd887e55b0537c2c8299ca29
sujitojha1 <payalchakraborty@Payals-MacBook-Air.local>
2020-03-17 02:30:34 +0530

    grad cam feature updates

--- Modules/grad_cam.py
+++ Modules/grad_cam.py
@@ -44,7 +44,7 @@ class ModelOutputs():
 
     def __init__(self, model, target_layers):
         self.model = model
-        self.feature_extractor = FeatureExtractor(self.model.features, target_layers)
+        self.feature_extractor = FeatureExtractor(self.model, target_layers)
 
     def get_gradients(self):
         return self.feature_extractor.gradients
