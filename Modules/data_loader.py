8a87aebe7bc85ee7792161e3d2c25ec7abd59e3b
sujitojha1 <payalchakraborty@Payals-MacBook-Air.local>
2020-03-17 19:15:32 +0530

    train and test

--- Modules/data_loader.py
+++ Modules/data_loader.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from albumentations import Compose, RandomCrop, Normalize, HorizontalFlip, Resize, Cutout
 from albumentations.pytorch import ToTensor
 
-class album_Compose():
+class album_Compose_train():
     def __init__(self):
         self.albumentations_transform = Compose([
             RandomCrop(30,30),
@@ -21,12 +21,23 @@ class album_Compose():
         img = self.albumentations_transform(image=img)['image']
         return img
 
+class album_Compose_test():
+    def __init__(self):
+        self.albumentations_transform = Compose([
+            Normalize(mean=[0.4914, 0.4822, 0.4465],std=[.2023, 0.1994, 0.2010]),
+            ToTensor()
+        ])
+    def __call__(self,img):
+        img = np.array(img)
+        img = self.albumentations_transform(image=img)['image']
+        return img
+
 class dataset_cifar10:
     """
     Class to load the data and define the data loader
     """
 
-    def __init__(self, sample_batch_size=5, batch_size=128):
+    def __init__(self, batch_size=128):
 
         # Defining CUDA
         cuda = torch.cuda.is_available()
@@ -35,73 +46,37 @@ class dataset_cifar10:
         # Defining data loaders with setting
         self.dataloaders_args = dict(shuffle=True, batch_size = batch_size, num_workers = 4, pin_memory = True) if cuda else dict(shuffle=True,batch_size = batch_size)
         self.sample_dataloaders_args = self.dataloaders_args.copy()
-        self.sample_dataloaders_args['batch_size'] = sample_batch_size
 
         self.classes = ('plane', 'car', 'bird', 'cat',
             'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
+    def data(self, train_flag):
 
-    
+        # Transformations data augmentation (only for training)
+        if train_flag :
+            return datasets.CIFAR10('./Data',
+                            train=train_flag,
+                            transform=album_Compose_train(),
+                            download=True)
 
-
-    def data(self, train_flag , trnsfm_flag=True):
-
-        trnsfm_list = [ToTensor()]
-
-        if trnsfm_flag:
-
-            # Transformations data augmentation (only for training)
-            if train_flag :
-                return datasets.CIFAR10('./Data',
+        # Testing transformation - normalization adder
+        else:
+            return datasets.CIFAR10('./Data',
                                 train=train_flag,
-                                transform=album_Compose(),
-                                download=True)
-
-            # Testing transformation - normalization adder
-            trnsfm_list.append(Normalize(mean=[0.4914, 0.4822, 0.4465],std=[.2023, 0.1994, 0.2010]))
-
-        trnsfm = transforms.Compose(trnsfm_list)
-        # Loading data
-        return datasets.CIFAR10('./Data',
-                                train=train_flag,
-                                transform=trnsfm,
+                                transform=album_Compose_test(),
                                 download=True)
 
     # Dataloader function
     def loader(self, train_flag=True):
         return(torch.utils.data.DataLoader(self.data(train_flag), **self.dataloaders_args))
 
-    # Sample Dataloader Function
-    def sample_loader(self, train_flag=True):
-        return(torch.utils.data.DataLoader(self.data(train_flag,trnsfm_flag=False), **self.sample_dataloaders_args))
 
     def data_summary_stats(self):
         # Load train data as numpy array
-        train_data = self.data(train_flag=True,trnsfm_flag=False).data
-        test_data = self.data(train_flag=False,trnsfm_flag=False).data
+        train_data = self.data(train_flag=True).data
+        test_data = self.data(train_flag=False).data
 
         total_data = np.concatenate((train_data, test_data), axis=0)
         print(total_data.shape)
         print(total_data.mean(axis=(0,1,2))/255)
         print(total_data.std(axis=(0,1,2))/255)
-
-    def sample_pictures(self, train_flag=True , return_flag = False):
-
-        # get some random training images
-        dataiter = iter(self.sample_loader(train_flag))
-        images,labels = dataiter.next()
-
-        fig = plt.figure(figsize=(10, 5))
-
-        # Show images
-        for idx in np.arange(len(labels.numpy())):
-                ax = fig.add_subplot(5, 5, idx+1, xticks=[], yticks=[])
-                npimg = np.transpose(images[idx].numpy(),(1,2,0))
-                ax.imshow(npimg, cmap='gray')
-                ax.set_title("Label={}".format(str(self.classes[labels[idx]])))
-
-        fig.tight_layout()  
-        plt.show()
-
-        if return_flag:
-            return images,labels
