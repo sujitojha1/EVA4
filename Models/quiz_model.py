e8fac4ab0de4d5b7ff99915056b58be9799c6c71
sujitojha1 <payalchakraborty@Payals-MacBook-Air.local>
2020-03-24 02:02:16 +0530

    changed concate to add

--- Models/quiz_model.py
+++ Models/quiz_model.py
@@ -27,21 +27,21 @@ class DNN(nn.Module):
 
         # Input layer - x1
         self.b1 = nn.Sequential(
-            nn.Conv2d(3, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(True),
         )
         
         # x2 = conv(x1)
         self.b2 = nn.Sequential(
-            nn.Conv2d(128, 64, kernel_size=1),
+            nn.Conv2d(64, 64, kernel_size=1),
             nn.BatchNorm2d(64),
             nn.ReLU(True),
         )
 
         # x3 = conv(x1 + x2)
         self.b3 = nn.Sequential(
-            nn.Conv2d(192, 64, kernel_size=3, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(True),
         )
@@ -51,61 +51,61 @@ class DNN(nn.Module):
 
         # x5 = conv(x4)
         self.b4 = nn.Sequential(
-            nn.Conv2d(256, 128, kernel_size=1),
+            nn.Conv2d(64, 128, kernel_size=1),
             nn.BatchNorm2d(128),
             nn.ReLU(True),
         )
 
         # x6 = conv(x4 + x5)
         self.b5 = nn.Sequential(
-            nn.Conv2d(384, 128, kernel_size=3, padding=1),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(True),
         )
 
         # x7 = conv(x4 + x5 + x6)
         self.b6 = nn.Sequential(
-            nn.Conv2d(512, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(True),
         )
 
         # x9 = conv(x8)
         self.b7 = nn.Sequential(
-            nn.Conv2d(512, 256, kernel_size=1),
+            nn.Conv2d(128, 256, kernel_size=1),
             nn.BatchNorm2d(256),
             nn.ReLU(True),
         )
 
         # x10 = conv(x8 + x9)
         self.b8 = nn.Sequential(
-            nn.Conv2d(768, 256, kernel_size=3, padding=1),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(True),
         )
 
         # x11 = conv(x8 + x9 + x10)
         self.b9 = nn.Sequential(
-            nn.Conv2d(1024, 1024, kernel_size=3, padding=1),
-            nn.BatchNorm2d(1024),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(True),
         )
 
         self.avgpool = nn.AvgPool2d(8, stride=1)
-        self.linear = nn.Linear(1024, 10)
+        self.linear = nn.Linear(256, 10)
 
     def forward(self, x):
         x1 = self.b1(x)
         x2 = self.b2(x1)
-        x3 = self.b3(torch.cat([x1,x2], 1))
-        x4 = self.maxpool(torch.cat([x1,x2,x3], 1))
+        x3 = self.b3(x+x2)
+        x4 = self.maxpool(x1+x2+x3)
         x5 = self.b4(x4)
-        x6 = self.b5(torch.cat([x4,x5], 1))
-        x7 = self.b6(torch.cat([x4,x5,x6], 1))
-        x8 = self.maxpool(torch.cat([x5,x6,x7], 1))
+        x6 = self.b5(x4+x5)
+        x7 = self.b6(x4+x5+x6)
+        x8 = self.maxpool(x5+x6+x7)
         x9 = self.b7(x8)
-        x10 = self.b8(torch.cat([x8,x9], 1))
-        x11 = self.b9(torch.cat([x8,x9,x10], 1))
+        x10 = self.b8(x8+x9)
+        x11 = self.b9(x8+x9+x10)
         x12 = self.avgpool(x11)
         x13 = x12.view(x12.size(0), -1)
         out = self.linear(x13)
