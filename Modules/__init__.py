2812d2cb686a7d0c400ab28e4e6ba019c3e1f498
sujitojha1 <payalchakraborty@Payals-MacBook-Air.local>
2020-04-12 03:47:15 +0530

    initialization

--- Modules/__init__.py
+++ Modules/__init__.py
@@ -4,3 +4,15 @@ from .test_model import *
 from .grad_cam import *
 from .plotting import *
 from .lr_finder import LRFinder
+
+# Future print function
+from __future__ import print_function
+
+# Installing latest Albumentation library
+!pip install -U git+https://github.com/albu/albumentations -q --quiet
+#pip install apex -q
+
+import matplotlib.pyplot as plt
+
+# For inline matplotlib plotting
+%matplotlib inline
