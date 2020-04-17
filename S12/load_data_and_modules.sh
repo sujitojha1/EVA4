5bc33397a678db3e963ea73d5befd09e52f2b6af
sujitojha1 <payalchakraborty@Payals-MacBook-Air.local>
2020-04-18 03:27:52 +0530

    echo

--- S12/load_data_and_modules.sh
+++ S12/load_data_and_modules.sh
@@ -1,15 +1,18 @@
 # Load modules/models from EVA course
 rm -rf EVA4
-git clone -b rev4 https://github.com/sujitojha1/EVA4 -q --quiet
-echo "Loaded EVA4 Modules and Models"
+git clone -b rev5 https://github.com/sujitojha1/EVA4 -q --quiet
+echo "Loaded EVA4 Modules"
 
 # Installing latest Albumentation library
 pip install -U git+https://github.com/albu/albumentations -q --quiet
-#pip install apex -q
+echo "Pip installation of Albumentation done !!"
 
-echo "Loaded EVA4 Modules and Models"
+echo "Downloading data"
+set echo off
 wget -nc http://cs231n.stanford.edu/tiny-imagenet-200.zip
 unzip tiny-imagenet-200.zip
+
+set echo on
 # rm -r ./tiny-imagenet-200/test
 # python3 val_format.py
 # find . -name "*.txt" -delete
