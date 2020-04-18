63aff6554951671f3f7563326ebb7ce71846b91e
sujitojha1 <payalchakraborty@Payals-MacBook-Air.local>
2020-04-18 16:10:00 +0530

    validation additional images

--- S12/val_format.py
+++ S12/val_format.py
@@ -4,6 +4,9 @@ import os
 from shutil import move
 from os.path import join
 from os import listdir, rmdir
+import random
+random.seed(128)
+
 
 target_folder = './tiny-imagenet-200/val/'
 #test_folder   = './tiny-imagenet-200/test/'
@@ -31,3 +34,19 @@ for path in paths:
     move(path, dest)
     
 rmdir('./tiny-imagenet-200/val/images')
+
+path = 'tiny-imagenet-200/'
+list1 = range(0, 500)
+
+id_list = []
+for i, line in enumerate(open(path + 'wnids.txt', 'r')):
+    id_list.append(line.replace('\n', ''))
+
+for id in id_list:
+  print(id)
+  sample_list = random.sample(list1, k=135)
+  source_files = ['./tiny-imagenet-200/train/'+str(id)+'/images/'+str(id)+'_'+str(file_num)+'.JPEG' for file_num in sample_list]
+  dest_files = ['./tiny-imagenet-200/val/'+str(id)+'/images/'+str(id)+'_'+str(file_num)+'.JPEG' for file_num in sample_list]
+
+  for (src_file, dest_file) in zip(source_files, dest_files): 
+     move(src_file, dest_file)
