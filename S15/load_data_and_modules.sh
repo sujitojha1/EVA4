b7b99d042dcb53c038cc85500b62f99241cae9fc
sujitojha1 <payalchakraborty@Payals-MacBook-Air.local>
2020-06-02 02:54:11 +0530

    load script

--- S15/load_data_and_modules.sh
+++ S15/load_data_and_modules.sh
@@ -6,6 +6,7 @@ echo "Pip installation of Albumentation done !!"
 
 mkdir 'dataset'
 
+echo "Copying files from google drive to runtime !!"
 cp -r '/content/drive/My Drive/EVA4/S15/fg_bg.zip' dataset/fg_bg.zip 
 cp -r '/content/drive/My Drive/EVA4/S15/mask.zip' dataset/mask.zip 
 cp -r '/content/drive/My Drive/EVA4/S15/depth_map.zip' dataset/depth_map.zip 
